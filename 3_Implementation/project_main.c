5bb43da2008455d88d79c15fc115497b221aef93
Shirishalavudya <99073372+Shirishalavudya@users.noreply.github.com>
2022-03-01 22:21:45 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,11 +1,11 @@
 /**
  * @file main1.c
- * @author Pavan
+ * @author Lavudya Shirisha
  * @brief 
  * @version 0.1
- * @date 2021-09-07
+ * @date 2022-02-22
  * 
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2022
  * 
  */
 #include"stdio.h"
